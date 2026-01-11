2edd91f8025914d5a3b2e3d6343e4fbc3219d009
Amongety <aleshka203_shkapovv@mail.ru>
2026-01-11 22:18:42 +0300

    Добавлена функция panic для отладочной информации

--- include/kernelSpace/libsbi/sci.h
+++ include/kernelSpace/libsbi/sci.h
@@ -8,6 +8,15 @@
 #include "le.h"
 #include "dbcn.h"
 
+extern void halt(void);
+
 sbi_ret_t sbi_console_printf(const char* format, ...);
+
+#define PANIC(format, ...)											\
+	do {													\
+		sbi_console_printf("PANIC: %s:%d: " format "\n", __FILE__, __LINE__, ##__VA_ARGS__);		\
+														\
+		halt();												\
+	} while(0)
 
 #endif
--- kernelSpace/kernel.c
+++ kernelSpace/kernel.c
@@ -1,24 +1,25 @@
 #include "../include/kernelSpace/config.h"
 #include "../include/kernelSpace/libsbi/sci.h"
 
-void main()
+void main() 
 {
-	sbi_console_printf("Hello debug SBI!\n");
-	
-	/*
-	for(int i = 0; i < 5; ++i) {
-		sbi_console_putchar("Test\n"[i]);
-	}
+        sbi_console_printf("Hello debug SBI!\n");
+	PANIC("booted");
 
-	sbi_debug_console_write_byte('a');
+        /*
+        for(int i = 0; i < 5; ++i) {
+                sbi_console_putchar("Test\n"[i]);
+        }
 
-	unsigned char test[8];
-	struct sbiret ret = sbi_debug_console_read(6, (unsigned long*)test);
-	if(ret.uvalue == 6) {
-		test[6] = '!';
-		test[7] = '\n';
-		sbi_debug_console_write(8, (unsigned long*)test);
-	}
-	*/
+        sbi_debug_console_write_byte('a');
+
+        unsigned char test[8];
+        struct sbiret ret = sbi_debug_console_read(6, (unsigned long*)test);
+        if(ret.uvalue == 6) {
+                test[6] = '!';
+                test[7] = '\n';
+                sbi_debug_console_write(8, (unsigned long*)test);
+        }
+        */
 
 }
